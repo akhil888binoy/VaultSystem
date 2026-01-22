4dee0d2238346cde4b7d4c8d6711930b5e1158a3
akhilbinoy <akhilbinoy@akhilbinoys-MacBook-Air.local>
2026-01-22 18:24:48 +0530

    feat: add verify

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,8 @@ echo "------------------------------------------"
 forge script script/DeployScript.s.sol:DeployScript \
   --rpc-url "$BASE_MAINNET_RPC_URL" \
   --broadcast \
+  --verify \
+  --etherscan-api-key "$BASESCAN_API_KEY" \
   --legacy
 
 echo "------------------------------------------"
